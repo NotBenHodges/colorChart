7528f398b1699a8894e1210f3623112adc8060b2
Ben Hodges <ben.hodges@centre.edu>
2019-03-05 14:40:06 -0500

    push

---
+++ chart.js
@@ -0,0 +1,41 @@
+var colorDataP = d3.csv("colors.csv");
+var drawChart = function(data)
+{
+  var height = 400;
+  var width = 600;
+  var barWidth = width/data.length;
+  var svg = d3.select("body")
+              .append("svg")
+              .attr("height",height)
+              .attr("width",width);
+  svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x",function(d,i)
+      {
+        return i * barWidth;
+      })
+      .attr("y",function(d) {
+        return height - d.num * 10;
+      })
+      .attr("width",barWidth)
+      .attr("height",function(d)
+      {
+        return d.num * 10;
+      })
+      .attr("fill",function(d)
+    {
+      return d.color;
+    })
+}
+
+colorDataP.then(function(data)
+{
+  console.log("data", data);
+  drawChart(colorDataP);
+},
+  funciton(err)
+{
+  console.log(err);
+})
